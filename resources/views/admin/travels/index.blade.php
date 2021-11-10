2337a6cc093259a6871d1c81eb830785a0f22558
Sabelius <sbebby92@virgilio.it>
2021-11-10 17:22:41 +0100

    aggiornamenti finali

--- resources/views/admin/travels/index.blade.php
+++ resources/views/admin/travels/index.blade.php
@@ -3,23 +3,25 @@
 
 @section("content")
 <div class="container">
-    <div class="row">
-        <h1 class="pt-2 pl-4 text-uppercase">Travels:</h1>
+    <div class="row justify-content-center">
+        <h1 class="pt-2 text-uppercase">Travels:</h1>
     </div>
-    <div class="row">
-        @foreach ($travels as $travel)
-            <div class="travel-list pt-5">
-                <ul>
-                    <li>
-                        <h3>{{$travel->journey_name}}</h3>
-                        <p>{{$travel->description}}</p>
-                    </li>
-                </ul>
-            </div>
-            <div class="travel-main-page pl-4 pt-2">
-                <a href="{{route("admin.travels.show", $travel)}}" class="btn btn-success">Vai alla pagina di dettaglio</a>
-            </div>
-        @endforeach
+    <div class="row justify-content-center">
+        <div class="col-12">
+            @foreach ($travels as $travel)
+                <div class="travel-list pt-5">
+                    <ul>
+                        <li>
+                            <h3>{{$travel->journey_name}}</h3>
+                            <p>{{$travel->description}}</p>
+                        </li>
+                    </ul>
+                </div>
+                <div class="travel-main-page pt-2 d-flex justify-content-center">
+                    <a href="{{route("admin.travels.show", $travel)}}" class="btn btn-success">Vai alla pagina di dettaglio</a>
+                </div>
+            @endforeach
+        </div>
     </div>
 </div>
 @endsection
--- resources/views/admin/travels/show.blade.php
+++ resources/views/admin/travels/show.blade.php
@@ -3,25 +3,27 @@
 
 @section("content")
 <div class="container">
-    <div class="row">
-        <h1 class="pt-2 pl-4 text-uppercase">Travel:</h1>
+    <div class="row justify-content-center">
+        <h1 class="pt-2 text-uppercase">Travel:</h1>
     </div>
-    <div class="row">
-        <div class="travel-list pt-3">
-            <ul>
-                 <li>
-                    <h3>{{$travel->journey_name}}</h3>
-                    <p>{{$travel->description}}</p>
-                    <h5>Data partenza: <span>{{$travel->departure_date}}</span></h5>
-                    <h5>Data ritorno:  <span>{{$travel->return_date}}</span></h5>
-                    <h5>Posti disponibili:  <span>{{$travel->available_places}}</span></h5>
-                    <h5>Posti rimanenti:  <span>{{$travel->is_available}}</span></h5>
-                    <h5>Prezzo viaggio:  <span>{{$travel->price}}</span> €</h5>
-                </li>
-            </ul>
-        </div>
-        <div class="travel-main-page pl-4 pt-2">
-            <a href="{{route("admin.travels.index")}}" class="btn btn-success">Torna alla pagina dei viaggi</a>
+    <div class="row justify-content-center">
+        <div class="col-12">
+            <div class="travel-list pt-5">
+                <ul>
+                    <li>
+                        <h3>{{$travel->journey_name}}</h3>
+                        <p>{{$travel->description}}</p>
+                        <h5>Data partenza: <span>{{$travel->departure_date}}</span></h5>
+                        <h5>Data ritorno:  <span>{{$travel->return_date}}</span></h5>
+                        <h5>Posti disponibili:  <span>{{$travel->available_places}}</span></h5>
+                        <h5>Posti rimanenti:  <span>{{$travel->is_available}}</span></h5>
+                        <h5>Prezzo viaggio:  <span>{{$travel->price}}</span> €</h5>
+                    </li>
+                </ul>
+            </div>
+            <div class="travel-main-page pt-2 d-flex justify-content-center">
+                <a href="{{route("admin.travels.index")}}" class="btn btn-success">Torna alla pagina dei viaggi</a>
+            </div>
         </div>
     </div>
 </div>
